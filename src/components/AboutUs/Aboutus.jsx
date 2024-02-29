1138319ac8ca555e1ec2e0472b2426c3f59162de
Daria <dargrab07@gmail.com>
2024-02-29 14:14:35 +0100

    populated Person component and styled

--- src/components/AboutUs/Aboutus.jsx
+++ src/components/AboutUs/Aboutus.jsx
@@ -1,7 +1,6 @@
-import { nanoid } from "nanoid";
 import styles from "./AboutUs.module.scss";
-import { persons } from "../../data/personOptions";
 import clsx from "clsx";
+import Person from "./Person/Person";
 
 const AboutUs = () => {
   return (
@@ -9,21 +8,7 @@ const AboutUs = () => {
       <div className={clsx("container", styles.aboutUsWrapper)}>
         <h1 className={styles.aboutUsTitle}>Nasi specjaliści</h1>
         <ul className={styles.aboutUsList}>
-          {persons.map((person) => (
-            <li key={nanoid()} className={styles.listItem}>
-              <div className={styles.person}>
-                <img
-                  className={styles.personImg}
-                  src={person.image}
-                  alt={person.name}
-                />
-              </div>
-              <div className={styles.personDetail}>
-                <p className={styles.personName}>{person.name}</p>
-                <p className={styles.personDescription}>{person.description}</p>
-              </div>
-            </li>
-          ))}
+          <Person />
         </ul>
       </div>
     </section>
--- src/components/AboutUs/Person/Person.jsx
+++ src/components/AboutUs/Person/Person.jsx
@@ -1,5 +1,28 @@
+import { persons } from "data/personOptions";
+import { nanoid } from "nanoid";
+import styles from "./Person.module.scss";
+
 const Person = () => {
-  return null;
+  return (
+    <>
+      {persons.map((person) => (
+        <li key={nanoid()} className={styles.listItem}>
+          <div className={styles.person}>
+            <img
+              className={styles.personImg}
+              src={person.image}
+              alt={person.name}
+            />
+          </div>
+          <div className={styles.personDetail}>
+            <p className={styles.personName}>{person.name}</p>
+            <p className={styles.personDescription}>{person.description}</p>
+          </div>
+        </li>
+      ))}
+      ;
+    </>
+  );
 };
 
 export default Person;
