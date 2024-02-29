0d21dc7f1fe0ccd5bbc759dbc27d1cdf456ee63c
Daria <dargrab07@gmail.com>
2024-02-29 15:34:33 +0100

    fixed another typo for AboutUs in import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbar/Navbar";
 import Homepage from "components/Homepage/Homepage";
-import AboutUs from "components/Aboutus/AboutUs";
+import AboutUs from "components/AboutUs/AboutUs";
 import Offer from "components/Offer/Offer";
 
 const App = () => {
